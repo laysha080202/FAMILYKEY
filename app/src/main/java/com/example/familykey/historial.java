d1bcb014ffd0bfa7231e3827f9bb0b2d6d70110b
laysha080202 <92221619+laysha080202@users.noreply.github.com>
2021-10-10 08:59:55 -0600

    diseño de interfaces
    realizamos el diseño de nuestras interfaces de acuerdo a las especificaciones que dirigia el diseñador grafico

--- app/src/main/java/com/example/familykey/historial.java
+++ app/src/main/java/com/example/familykey/historial.java
@@ -2,13 +2,62 @@ package com.example.familykey;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class historial extends AppCompatActivity {
+    Button bt_you,bt_play,bt_google,bt_back,bt_conex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_historial);
+
+        bt_you=findViewById(R.id.bt_you);
+        bt_play=findViewById(R.id.bt_play);
+        bt_google=findViewById(R.id.bt_goo);
+        bt_back=findViewById(R.id.bt_back1);
+        bt_conex=findViewById(R.id.bt_conex1);
+
+        bt_you.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(historial.this,paginaprincipal.class);
+                startActivity(i);
+            }
+        });
+
+        bt_play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(historial.this,paginaprincipal.class);
+                startActivity(i);
+            }
+        });
+
+        bt_google.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(historial.this,paginaprincipal.class);
+                startActivity(i);
+            }
+        });
+        bt_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(historial.this,paginaprincipal.class);
+                startActivity(i);
+            }
+        });
+
+      bt_conex.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(historial.this,paginaprincipal.class);
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/familykey/juegos.java
+++ app/src/main/java/com/example/familykey/juegos.java
@@ -2,13 +2,63 @@ package com.example.familykey;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class juegos extends AppCompatActivity {
+    Button bt_free,bt_clash,bt_clan,bt_back1,bt_conex1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_juegos);
+        bt_free=findViewById(R.id.FREE FIRE);
+        bt_clash=findViewById(R.id.CLASH_ROLAYE);
+        bt_clan=findViewById(R.id.CLASH_OF_CLAN);
+        bt_back1=findViewById(R.id.ATRAS1);
+        bt_conex1=findViewById(R.id.CONEXION1);
     }
+    @Override
+    public void onClick(View view) {
+        Intent i = new Intent(juegos.this, paginaprincipal.class);
+        startActivity(i);
+    }
+      bt_free.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent i = new Intent(juegos.this, paginaprincipal.class);
+            startActivity(i);
+        }
+    }
+      bt_clash.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent i = new Intent(juegos.this, paginaprincipal.class);
+            startActivity(i);
+        }
+    }
+    bt_clan.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent i = new Intent(juegos.this, paginaprincipal.class);
+            startActivity(i);
+        }
+    }
+    bt_back1.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent i = new Intent(juegos.this, paginaprincipal.class);
+            startActivity(i);
+        }
+    }
+    bt_conex1.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent i = new Intent(juegos.this, paginaprincipal.class);
+            startActivity(i);
+        }
+    });
 }
+
